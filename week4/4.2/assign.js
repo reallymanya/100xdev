1a2b81022c198c28afc5b4ad401026786889aa53
reallymanya <manyatakkar.01@gmail.com>
2025-09-13 01:21:04 +0530

    learning CRUD in EXPRESS

---
+++ week4/4.2/assign.js
@@ -0,0 +1,101 @@
+/*You need to create 4 routes (4 things that the hospital can do)
+1. GET - User can check how many kidneys they have and their health
+2. POST - User can add a new kidney
+3. PUT - User can replace a kidney, make it healthy
+4. DELETE - User can remove a kidney*/
+
+import express from "express";
+
+const app = express();
+
+var users = [
+  {
+    name: "John",
+    kidneys: [
+      {
+        healthy: false,
+      },
+    ],
+  },
+];
+
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  const johnKidneys = users[0].kidneys;
+  const numofKidneys = johnKidneys.length;
+  let numofHealthyKindeys = 0;
+
+  for (let i = 0; i < johnKidneys.length; i++) {
+    if (johnKidneys[i].healthy) {
+      numofHealthyKindeys = numofHealthyKindeys + 1;
+    }
+  }
+  const numofUnhealthyKidneys = numofKidneys - numofHealthyKindeys;
+
+  res.json({
+    numofKidneys,
+    numofHealthyKindeys,
+    numofUnhealthyKidneys,
+  });
+});
+
+app.post("/", function (req, res) {
+  let isHealthy = req.body.isHealthy;
+  users[0].kidneys.push({
+    healthy: isHealthy,
+  });
+
+  res.json({
+    msg: "Done",
+  });
+});
+
+//converts unhealthy to healthy
+
+app.put("/", function (req, res) {
+    if (isthereatleastoneunhealthykidney()) {
+  for (let i = 0; i < users[0].kidneys.length; i++) {
+    users[0].kidneys[i].healthy = true;
+  }
+  res.json({});
+} else{
+    res.status(411).json({
+      msg: "You have no bad kidneys",
+    });
+}
+});
+
+//removing all the unhealthy kidneys
+//only if atleast one unhealthy kidney is there, do this else return 411
+
+function isthereatleastoneunhealthykidney() {
+  let atleastoneunhealthykidney = false;
+
+  for (let i = 0; i < users[0].kidneys.length; i++) {
+    if (!users[0].kidneys[i].healthy) {
+      atleastoneunhealthykidney = true;
+    }
+  }
+  return atleastoneunhealthykidney;
+}
+app.delete("/", function (req, res) {
+  if (isthereatleastoneunhealthykidney()) {
+    const newKidneys = [];
+    for (let i = 0; i < users[0].kidneys.length; i++) {
+      if (users[0].kidneys[i].healthy) {
+        newKidneys.push({
+          healthy: true,
+        });
+      }
+    }
+    users[0].kidneys = newKidneys;
+    res.json({ msg: "done" });
+  } else {
+    res.status(411).json({
+      msg: "You have no bad kidneys",
+    });
+  }
+});
+
+app.listen(3000);
---
+++ week4/4.2/index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+
+const app = express()
+
+function sum(n){
+  let ans = 0;
+  for(let i = 1; i<=n;i++){
+    ans = ans + i;
+  }
+  return ans;
+}
+
+//route handlers
+app.get('/', (req, res) => {
+  const n = parseInt(req.query.n);
+  const ans = sum(n);
+  res.send('Hello World sum is ' + ans);
+})
+
+app.post('/', (req, res) => { 
+  res.send('Hello World from myside')
+})
+
+
+app.listen(3000) //which part
